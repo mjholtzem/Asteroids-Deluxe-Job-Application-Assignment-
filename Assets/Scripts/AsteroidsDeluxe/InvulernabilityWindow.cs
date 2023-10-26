6083971badfe02fdbd9b71c0dfe4b3510670e0fc
Matthew Holtzem <mjholtzem@gmail.com>
2023-10-26 15:23:37 -0600

    fixing invulnerability window bug

--- Assets/Scripts/AsteroidsDeluxe/InvulernabilityWindow.cs
+++ Assets/Scripts/AsteroidsDeluxe/InvulernabilityWindow.cs
@@ -2,26 +2,42 @@ using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class InvulernabilityWindow : MonoBehaviour
+namespace AsteroidsDeluxe
 {
-	[SerializeField][Min(0)] private float _invulnerabilityWindow = .5f;
-	[SerializeField] private Rigidbody2D _rigidbody;
-
-    private void OnEnable()
-    {
-		HandleInvulerabilityWindow();
-    }
-
-	private async void HandleInvulerabilityWindow()
+	[RequireComponent(typeof(Rigidbody2D))]
+	[RequireComponent(typeof(AsteroidsBehaviour))]
+	public class InvulernabilityWindow : MonoBehaviour
 	{
-		_rigidbody.simulated = false;
-		await Task.Delay(TimeSpan.FromSeconds(_invulnerabilityWindow));
-		_rigidbody.simulated = true;
-	}
+		[SerializeField][Min(0)] private float _invulnerabilityWindow = .5f;
+		[SerializeField] private Rigidbody2D _rigidbody;
+		[SerializeField] private AsteroidsBehaviour _asteroidsBehaviour;
 
-	private void Reset()
-	{
-		_rigidbody = GetComponent<Rigidbody2D>();
+		private bool _wasEnabled = false;
+
+		private void Update()
+		{
+			//detect when the root behaviour has been enabled and trigger invulnerability.
+			//this is somewhat on the hacky side and ideally we might want to use UniRx or something
+			//to actually just be notified of it but this is an ok way to go as well
+			if(_asteroidsBehaviour == null) return;
+
+			if(_wasEnabled == _asteroidsBehaviour.enabled) return;
+
+			_wasEnabled = _asteroidsBehaviour.enabled;
+			if(_asteroidsBehaviour.enabled) HandleInvulerabilityWindow();
+		}
+
+		private async void HandleInvulerabilityWindow()
+		{
+			_rigidbody.simulated = false;
+			await Task.Delay(TimeSpan.FromSeconds(_invulnerabilityWindow));
+			_rigidbody.simulated = true;
+		}
+
+		private void Reset()
+		{
+			_rigidbody = GetComponent<Rigidbody2D>();
+			_asteroidsBehaviour = GetComponent<AsteroidsBehaviour>();
+		}
 	}
 }
